2411cafdd44e8ac680f89410e1026477f1c75e85
Thuy Nguyen <thanhthuynguyendn94@gmail.com>
2020-09-15 15:51:53 +0700

    Done TC06_Xpath

--- selenium/webdriver/Topic03_Xpath_CSS_Part1.java
+++ selenium/webdriver/Topic03_Xpath_CSS_Part1.java
@@ -91,66 +91,110 @@ public class Topic03_Xpath_CSS_Part1 {
 		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("thuy@mailinator.com");
 		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("123456");
 		driver.findElement(By.xpath("//button[@id='send2']")).click();
-	
-		
 
 		String myDashboardText = driver.findElement(By.xpath("//div[@class='page-title']//h1")).getText();
 		Assert.assertEquals(myDashboardText, "MY DASHBOARD");
-		
+
 		String contactInfomation = driver.findElement(By.xpath("//div[@class='welcome-msg']//strong")).getText();
 		Assert.assertEquals(contactInfomation, "Hello, Thuy Thanh Ng!");
-		
+
 		String contactInfomationText = driver.findElement(By.xpath("//div[@class='col-1']//div[@class='box-content']//p")).getText();
-		//Assert.assertEquals(contactInfomationText, "Thuy Thanh Ng");
-		//Assert.assertEquals(contactInfomationText, "thuy@mailinator.com");//
-		
+		// Assert.assertEquals(contactInfomationText, "Thuy Thanh Ng");
+		// Assert.assertEquals(contactInfomationText, "thuy@mailinator.com");//
+
 		Assert.assertTrue(contactInfomationText.contains("Thuy Thanh Ng"));
 		Assert.assertTrue(contactInfomationText.contains("thuy@mailinator.com"));
-		
-		
-		//driver.findElement(By.xpath("//a[@class='skip-link skip-account skip-active']//span[@class='label']")).click();
+
+		// driver.findElement(By.xpath("//a[@class='skip-link skip-account
+		// skip-active']//span[@class='label']")).click();
 		driver.findElement(By.xpath("//span[@class='label' and text()='Account']")).click();
+
+		driver.findElement(By.xpath("//a[@title='Log Out']")).click();
+
+		// Cach1: text ngắn, ko có kí tự xuống dòng, tab hoặc khoảng trắng ở đầu cuối
+		// chuỗi
+		// Assert.assertTrue(driver.findElement(By.xpath("//div[@class='page-title']/h1[text()='My
+		// Dashboard')]")).isDisplayed());
+
+		// Cach2: tương đối: text dài, có kí tự xuống dòng, tab hoặc khoảng trắng ở đầu
+		// cuối chuỗi
+
+		// Assert.assertTrue(driver.findElement(By.xpath("//div[@class='page-title']/h1[contains(text(),'My
+		// Dashboard')]")).isDisplayed());
+
+		// Assert.assertEquals(driver.findElement(By.xpath("//div[@class='welcome-msg']//strong")).getText(),
+		// "Hello, Thuy Thanh Ng!");
+
+		// Tuong doi:
+		// String contactInfomationText =
+		// driver.findElement(By.xpath("//div[@class='col-1']//div[@class='box-content']//p")).getText();
+
+		// Assert.assertTrue(contactInfomationText.contains("Thuy Thanh Ng"));
+		// Assert.assertTrue(contactInfomationText.contains("thuy@mailinator.com"));
+
+		// Assert.assertTrue(driver.findElement(By.xpath("//div[@class='col-1']//div[@class='box-content']//p[contains(.,'Thuy
+		// Thanh Ng')]")).isDisplayed());
+		// Assert.assertTrue(driver.findElement(By.xpath("//div[@class='col-1']//div[@class='box-content']//p[contains(.,'thuy@mailinator.com')]")).isDisplayed());
+
+	}
+
+	@Test
+	public void TC_06_Register_A_New_Account_To_System() {
+		driver.get("http://live.demoguru99.com/index.php/customer/account/login/");
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+		driver.findElement(By.xpath("//a[@title='Create an Account']")).click();
+
+		String firstname = "Tom";
+		String lastname = "Jerry";
+		String email = "tomjerry123@mailinator.com";
 		
+		
+		driver.findElement(By.id("firstname")).sendKeys(firstname);
+		driver.findElement(By.id("lastname")).sendKeys(lastname);
+		driver.findElement(By.id("email_address")).sendKeys(email);
+		driver.findElement(By.id("password")).sendKeys("123456");
+		driver.findElement(By.id("confirmation")).sendKeys("123456");
+		
+		driver.findElement(By.xpath("//button[@title='Register']")).click();
+		
+		String registerSuccess = driver.findElement(By.xpath("//li[@class='success-msg']//span")).getText();
+		Assert.assertEquals(registerSuccess, "Thank you for registering with Main Website Store.");
+		
+		
+		String contactInfo = driver.findElement(By.xpath("//div[@class='col-1']//div[@class='box-content']//p")).getText();
+		
+		Assert.assertTrue(contactInfo.contains(firstname + " "+ lastname ));
+		Assert.assertTrue(contactInfo.contains(email));
+		
+		driver.findElement(By.xpath("//span[@class='label' and text()='Account']")).click();
+
 		driver.findElement(By.xpath("//a[@title='Log Out']")).click();
 		
 		
 		
-		
-
-		//Cach1: text ngắn, ko có kí tự xuống dòng, tab hoặc khoảng trắng ở đầu cuối chuỗi
-		//Assert.assertTrue(driver.findElement(By.xpath("//div[@class='page-title']/h1[text()='My Dashboard')]")).isDisplayed());
-		
-		
-		//Cach2: tương đối:  text dài,  có kí tự xuống dòng, tab hoặc khoảng trắng ở đầu cuối chuỗi
-		
-		//Assert.assertTrue(driver.findElement(By.xpath("//div[@class='page-title']/h1[contains(text(),'My Dashboard')]")).isDisplayed());
-		
-		
-		//Assert.assertEquals(driver.findElement(By.xpath("//div[@class='welcome-msg']//strong")).getText(), "Hello, Thuy Thanh Ng!");
-		
-		
-		//Tuong doi:
-		//String contactInfomationText =  driver.findElement(By.xpath("//div[@class='col-1']//div[@class='box-content']//p")).getText();
-		
-		//Assert.assertTrue(contactInfomationText.contains("Thuy Thanh Ng"));
-		//Assert.assertTrue(contactInfomationText.contains("thuy@mailinator.com"));
-		
-		
-		//Assert.assertTrue(driver.findElement(By.xpath("//div[@class='col-1']//div[@class='box-content']//p[contains(.,'Thuy Thanh Ng')]")).isDisplayed());
-		//Assert.assertTrue(driver.findElement(By.xpath("//div[@class='col-1']//div[@class='box-content']//p[contains(.,'thuy@mailinator.com')]")).isDisplayed());
-		
-	}
-
-	@Test
-	public void TC_06_Register_To_System() {
+//		driver.findElement(By.id("firstname")).sendKeys("QApro");
+//		driver.findElement(By.id("lastname")).sendKeys("Test");
+//		driver.findElement(By.id("email_address")).sendKeys("qaproteam@mailinator.com");
+//		driver.findElement(By.id("password")).sendKeys("123456");
+//		driver.findElement(By.id("confirmation")).sendKeys("123456");
+//		driver.findElement(By.xpath("//button[@title='Register']")).click();
+//		
+//		String registerSuccess = driver.findElement(By.xpath("//li[@class='success-msg']//span")).getText();
+//		Assert.assertEquals(registerSuccess, "Thank you for registering with Main Website Store.");
+//		
+//		
+//		String contactInfo = driver.findElement(By.xpath("//div[@class='col-1']//div[@class='box-content']//p")).getText();
+//		
+//		Assert.assertTrue(contactInfo.contains("QApro Test"));
+//		Assert.assertTrue(contactInfo.contains("qaproteam@mailinator.com"));
+//		
 
 	}
-	
+
 	@Test
 	public void TC_07_Register_To_System() {
 
 	}
-
 
 	@AfterClass
 	public void afterClass() {
